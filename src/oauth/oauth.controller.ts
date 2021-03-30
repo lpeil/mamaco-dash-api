6c1e3333e58666935fa56303329a5f4c3ac7846b
lpeil <luan@peil.dev>
2021-03-30 13:43:59 -0300

    create login response

--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { OauthService } from './oauth.service';
 
 @Controller('oauth')
 export class OauthController {
   constructor(private readonly oauthService: OauthService) {}
 
-  @Get('/login')
-  async login(@Query('code') code: string) {
-    console.log(code);
-
-    return { message: 'opa' };
+  @Post('/login')
+  async login(@Body() access) {
+    return await this.oauthService.login(access.accessToken, access.tokenType);
   }
 }
--- src/oauth/oauth.service.ts
+++ src/oauth/oauth.service.ts
@@ -1,4 +1,19 @@
+import axios from 'axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class OauthService {}
+export class OauthService {
+  async login(accessToken: string, tokenType: string) {
+    return await axios
+      .get('https://discord.com/api/users/@me', {
+        headers: {
+          authorization: `${tokenType} ${accessToken}`,
+        },
+      })
+      .then((res) => res.data)
+      .then((data) => {
+        return data;
+      })
+      .catch(console.error);
+  }
+}
